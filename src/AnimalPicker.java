39a5b56e6c0df4cf4ae6a68b58274b628267789f
NovaMatthew <mnova412@gmail.com>
2022-02-27 13:34:16 -0500

    user input and conditionals

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class AnimalPicker {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Which animal would you like to see: Dog or Cat?");
+		Scanner keyboard = new Scanner(System.in);
+		String userInput =  keyboard.nextLine();
+		
+		if (userInput.equalsIgnoreCase("cat"))
+			{
+			System.out.println("CAT ASCII ART HERE");
+			}
+		else if (userInput.equalsIgnoreCase("dog"))
+			{
+			System.out.println("Dog ASCII ART HERE");
+			}
 
+	
+		
 	}
 
 }
